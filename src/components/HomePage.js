8dbc427696761538f9b63f911b97e62ca8ee1512
geraldng1407 <g-e-r-a-l-d@hotmail.com>
2023-07-12 22:09:06 +0800

    Updated HomePage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,13 +1,28 @@
 import { MDBContainer } from "mdb-react-ui-kit";
-
+import Styles from "./Styles.module.css";
 export default function HomePage() {
     return (
-        <MDBContainer className="mt-5">
+        <MDBContainer className="mt-5 justify-content-center">
             <MDBContainer className="d-flex justify-content-center">
-                <h1> test test</h1>
+                <h1 className={Styles["home-page-title"]}>
+                    Exploring Cybersecurity and Machine Learning
+                </h1>
             </MDBContainer>
-            <MDBContainer className="d-flex justify-content-center">
-                <h2>subheading</h2>
+            <MDBContainer className="d-flex justify-content-center mt-3">
+                <h3 className="text-center mx-auto">
+                    Started with Software Engineering but want to explore the
+                    world of Cybersecurity and Machine Learning
+                </h3>
+            </MDBContainer>
+            {/* <MDBContainer className="d-flex justify-content-center mt-5">
+                <img
+                    src="avataaars.svg"
+                    alt="profilepicture"
+                    style={{ "max-width": "20%" }}
+                ></img>{" "}
+            </MDBContainer> */}
+            <MDBContainer className="d-flex justify-content-center mt-5">
+                <img src="computer.svg" alt="profilepicture"></img>{" "}
             </MDBContainer>
         </MDBContainer>
     );
