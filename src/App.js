ed109c5f6a1a9fa1b1098c2c2ba090205edba322
geraldng1407 <g-e-r-a-l-d@hotmail.com>
2023-07-26 22:14:11 +0800

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
+// import logo from "./logo.svg";
+// import "./App.css";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import HomePage from "./components/HomePage";
-import Navbar from "./components/Navbar";
+// import HomePage from "./components/HomePage";
+// import Navbar from "./components/Navbar";
+// import About from "./components/About"
+// import Projects from "./components/Projects"
+
+import Header from "./components/Header/Header";
+import About from "./components/About/About";
+import Projects from './components/Projects/Projects';
+import Footer from './components/Footer/Footer';
 
 function App() {
     return (
-      <div>
-        <Navbar/>
-        <HomePage/>
-      </div>
+        <div className="App">
+            <Header />
+            <About />
+            <Projects />
+            <Footer />
+
+        </div>
     );
 }
 
