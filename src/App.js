66c4fb4ed5b5677ad4bd5c0b799de14f60a6d5ce
geraldng1407 <g-e-r-a-l-d@hotmail.com>
2023-08-21 14:49:34 +0800

    Changed tab icon and text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,3 @@
-// import logo from "./logo.svg";
-// import "./App.css";
-
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// import HomePage from "./components/HomePage";
-// import Navbar from "./components/Navbar";
-// import About from "./components/About"
-// import Projects from "./components/Projects"
 
 import Header from "./components/Header/Header";
 import About from "./components/About/About";
